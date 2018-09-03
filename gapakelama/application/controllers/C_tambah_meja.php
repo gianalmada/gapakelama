00e503649fbe3c40246b9bcfc56ac12e2a045848
gian <gian.almada111@gmail.com>
2018-09-03 23:00:24 +0700

    UP4.2

---
+++ gapakelama/application/controllers/C_tambah_meja.php
@@ -0,0 +1,58 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard_Admin extends CI_Controller {
+
+  /**
+  * Index Page for this controller.
+  *
+  * Maps to the following URL
+  * 		http://example.com/index.php/welcome
+  *	- or -
+  * 		http://example.com/index.php/welcome/index
+  *	- or -
+  * Since this controller is set as the default controller in
+  * config/routes.php, it's displayed at http://example.com/
+  *
+  * So any other public methods not prefixed with an underscore will
+  * map to /index.php/welcome/<method_name>
+  * @see https://codeigniter.com/user_guide/general/urls.html
+  */
+  public function __construct(){
+    parent::__construct();
+    if($this->session->userdata('status') != "login"){
+      redirect("welcome/index");
+    }
+    $this->load->library('form_validation');
+
+  }
+
+function simpan(){
+        $nim=$this->input->post('nim');
+        $nama=$this->input->post('nama');
+        $prodi=$this->input->post('prodi');
+
+        $this->load->library('ciqrcode'); //pemanggilan library QR CODE
+
+        $config['cacheable']    = true; //boolean, the default is true
+        $config['cachedir']     = './assets/'; //string, the default is application/cache/
+        $config['errorlog']     = './assets/'; //string, the default is application/logs/
+        $config['imagedir']     = './assets/images/'; //direktori penyimpanan qr code
+        $config['quality']      = true; //boolean, the default is true
+        $config['size']         = '1024'; //interger, the default is 1024
+        $config['black']        = array(224,255,255); // array, default is array(255,255,255)
+        $config['white']        = array(70,130,180); // array, default is array(0,0,0)
+        $this->ciqrcode->initialize($config);
+
+        $image_name=$nim.'.png'; //buat name dari qr code sesuai dengan nim
+
+        $params['data'] = $nim; //data yang akan di jadikan QR CODE
+        $params['level'] = 'H'; //H=High
+        $params['size'] = 10;
+        $params['savename'] = FCPATH.$config['imagedir'].$image_name; //simpan image QR CODE ke folder assets/images/
+        $this->ciqrcode->generate($params); // fungsi untuk generate QR CODE
+
+        $this->mahasiswa_model->simpan_mahasiswa($nim,$nama,$prodi,$image_name); //simpan ke database
+        redirect('mahasiswa'); //redirect ke mahasiswa usai simpan data
+    }
+  }
